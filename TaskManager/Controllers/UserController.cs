cdb94f317aa5ab03ccd41ed32fe19e31ea6aa174
Phantomer-soft <kucuktas255@gmail.com>
2026-03-19 01:15:39 +0300

    update: updated register endpoint

--- TaskManager/Controllers/UserController.cs
+++ TaskManager/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaskManager.Data;
+using TaskManager.Models.Dtos;
 using TaskManager.Models.Entities;
 
 namespace TaskManager.Controllers;
@@ -16,15 +18,27 @@ public class UserController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Register(string email, string password)
+    public async Task<IActionResult> Register(RegisterDto request)
     {
-        User user = new User()
-        {
-            userName = email,
-            password = password
-        };
-        await _dbContext.AddAsync(user);
-        await _dbContext.SaveChangesAsync();
-        return Ok(user);
+            var isUsedMail = await _dbContext.Users.AnyAsync(x => x.email == request.email);
+            var isUsedUsername = await _dbContext.Users.AnyAsync(x => x.userName == request.userName);
+            
+            if (isUsedUsername || isUsedMail)// burada daha aciklayici bir mesaj eklenebilir ama bu sefer de guvenlik acisindan goze batan bir durum olur
+                return BadRequest(new { message = "Bu email veya kullanici adi kullaniliyor " });
+            
+            User user = new User()
+            {   
+                firstName = request.firstName,
+                lastName = request.lastName,
+                email = request.email,
+                userName = request.userName,
+                password = request.password  
+            
+            };
+            await _dbContext.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+            return Ok(user);
+          
+                    
     }
 }
