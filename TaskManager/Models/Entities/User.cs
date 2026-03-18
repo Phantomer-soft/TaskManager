ca1fd4f816fa6d3cc3bef0921b9a863830df6e05
Phantomer-soft <kucuktas255@gmail.com>
2026-03-19 01:14:45 +0300

    update: updated user entity

--- TaskManager/Models/Entities/User.cs
+++ TaskManager/Models/Entities/User.cs
@@ -1,8 +1,15 @@
-namespace TaskManager.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace TaskManager.Models.Entities;
+[Index(nameof(email), IsUnique = true)]
+[Index(nameof(userName), IsUnique = true)] 
 public class User
 {
+    
     public Guid id { get; set; } = Guid.NewGuid();
+    public string firstName { get; set; } =  string.Empty;
+    public string lastName { get; set; } =  string.Empty;
+    public string email { get; set; } =  string.Empty;
     public string userName { get; set; } = string.Empty;
     public string password { get; set; } = string.Empty;
 
