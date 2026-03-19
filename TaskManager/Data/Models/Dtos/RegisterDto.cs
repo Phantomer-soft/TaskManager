7f999adfad88d7af93d2d3eed6a1c0e89fd53e0c
Phantomer-soft <kucuktas255@gmail.com>
2026-03-19 23:09:32 +0300

    update: validation increased for register dto

--- TaskManager/Data/Models/Dtos/RegisterDto.cs
+++ TaskManager/Data/Models/Dtos/RegisterDto.cs
@@ -4,12 +4,15 @@ namespace TaskManager.Models.Dtos;
 
 public class RegisterDto
 {
+    [MinLength(4,ErrorMessage = "Kullanici adi en az 4 karakter olmalidir")]
     public string userName { get; set; } =  string.Empty;
     [MinLength(6, ErrorMessage = "Sifre en az 6 karakter olmalidir")]
     public string password { get; set; } =  string.Empty;
     [EmailAddress(ErrorMessage = "Lutfen gecerli bir email adresi giriniz")]
     public string email { get; set; } =  string.Empty;
+    [MinLength(2,ErrorMessage = "Ad en az 2 karakter olmalidir")]
     public string firstName { get; set; } =  string.Empty;
+    [MinLength(2,ErrorMessage = "Soyad en az 2 karakter olmalidir")]
     public string lastName { get; set; } =  string.Empty;
     
 }
