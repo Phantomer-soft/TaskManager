4ffe9b1b71ac01b51c010b64e12a0ac021cfd457
Phantomer-soft <kucuktas255@gmail.com>
2026-03-19 23:06:04 +0300

    add: refreshtoken table added

--- TaskManager/Data/ApiDbContext.cs
+++ TaskManager/Data/ApiDbContext.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using TaskManager.Models.Entities;
+using TaskManager.Auth;
+using TaskManager.Data.Models.Entities;
 
 namespace TaskManager.Data;
 
 public class ApiDbContext(DbContextOptions<ApiDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
     
 }
