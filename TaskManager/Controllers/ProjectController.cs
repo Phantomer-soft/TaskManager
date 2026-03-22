4d6cadad097107a24bb07b050fa3ece67b49551d
Phantomer-soft <kucuktas255@gmail.com>
2026-03-22 20:43:01 +0300

    add: added updateproject and deletetask endpoints

--- TaskManager/Controllers/ProjectController.cs
+++ TaskManager/Controllers/ProjectController.cs
@@ -29,6 +29,8 @@ public class ProjectController : Controller
         else
         {
             var user =  await _dbContext.Users.FirstOrDefaultAsync(u=> u.Id == Guid.Parse(userId));
+            if(user==null)
+                return  Unauthorized();
             var projectId = Guid.NewGuid();
             Project project = new()
             {
@@ -109,14 +111,49 @@ public class ProjectController : Controller
                     Description = pu.Project.Description,
                     Status = pu.Project.Status.ToString(),
                     Tasks = pu.Project.Tasks
-                });
+                })
+                .FirstOrDefault();
             if(project == null)
                 return NotFound(new {message = "Proje bulunamadi"});
             return Ok(project);
         }
         return Unauthorized();
     }
-    
+
+    [HttpPut("{projectId}")]
+    public async Task<IActionResult> UpdateProjectDetails(Guid projectId, [FromBody]UpdateProjectRequestDto request)
+    {
+        var  userId = User.FindFirst("UserId")?.Value;
+        if(userId == null)
+            return Unauthorized(new {message = "Lutfen tekrar giris yapiniz"});
+        if(!ModelState.IsValid)
+            return BadRequest(new {message = "Lutfen proje status degerini Ready, InProgress veya Done olarak ayarlayin "});
+        var projectUser = await _dbContext.ProjectUsers
+            .Include(pu => pu.Project)
+            .FirstOrDefaultAsync(pu => pu.UserId == Guid.Parse(userId)
+                                       && pu.ProjectId == projectId
+                                       && (pu.Role == Role.Admin || pu.Role == Role.Participant));
+        if (projectUser == null)
+        {
+            return Unauthorized(new {message =  "Proje bulunamadi veya yetkiniz yok"});
+        }
+        else
+        {
+            projectUser.Project.Description = request.Description ?? projectUser.Project.Description;
+            projectUser.Project.Header = request.Header ?? projectUser.Project.Header;
+            projectUser.Project.Status = request.ProjectStatus ?? projectUser.Project.Status;
+            await _dbContext.SaveChangesAsync();
+            SendProjectInfoDto dto = new()
+            {
+                ProjectId = projectUser.Project.Id,
+                Description = projectUser.Project.Description,
+                Header = projectUser.Project.Header,
+                Status = projectUser.Project.Status.ToString(),
+            };
+            return Ok(new {message = "Proje guncellendi",dto});
+        }
+    }
+    //                                   TASKS 
     [HttpPost("{projectId}/Tasks")]
     public async Task<IActionResult> CreateTask(Guid projectId,[FromBody]List<CreateTaskRequestDto> taskList)
     {
@@ -194,5 +231,44 @@ public class ProjectController : Controller
 
         await _dbContext.SaveChangesAsync();
         return Ok(task);
+    }
+
+    [HttpDelete("{taskId}/Tasks")]
+    public async Task<IActionResult> DeleteTask(Guid taskId)
+    {
+        var userId = User.FindFirst("UserId")?.Value;
+        if (userId == null)
+            return Unauthorized();
+        var task = await _dbContext.ProjectTasks
+            .FirstOrDefaultAsync(t => t.Id == taskId && !t.IsDeleted);
+        if (task == null)
+            return Unauthorized();
+        else
+        {
+            var projectUser = await _dbContext.ProjectUsers
+                .Include(pu => pu.Project)
+                .ThenInclude(p => p.Tasks)
+                .FirstOrDefaultAsync(pu => pu.UserId == Guid.Parse(userId)
+                                           && pu.ProjectId == task.ProjectId
+                                           && (pu.Role == Role.Admin || pu.Role == Role.Participant));
+
+            if (projectUser == null)
+                return Unauthorized(new { message = "Task bulunamadi veya yetkiniz yok" });
+
+            task.IsDeleted = true;
+            await _dbContext.SaveChangesAsync();
+
+            SendProjectInfoDto dto = new()
+            {
+                ProjectId = task.ProjectId,
+                Header = projectUser.Project.Header,
+                Description = projectUser.Project.Description,
+                Status = projectUser.Project.Status.ToString(),
+                Tasks = projectUser.Project.Tasks!.Where(t => !t.IsDeleted).ToList()
+            };
+
+            return Ok(dto);
+
+        }
     }
 }
