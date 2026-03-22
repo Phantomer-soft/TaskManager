7d1f49eab01de08885c5f767e73c329310354639
Phantomer-soft <kucuktas255@gmail.com>
2026-03-22 20:42:03 +0300

    update: required and error message added

--- TaskManager/Data/Models/Entities/ProjectTask.cs
+++ TaskManager/Data/Models/Entities/ProjectTask.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManager.Data.Models.Entities;
 
 public enum TaskStatus 
@@ -11,6 +13,8 @@ public class ProjectTask
 {
     public Guid Id { get; set; } =  Guid.NewGuid();
     public Guid ProjectId { get; set; }
+    [Required]
+    [MinLength(3, ErrorMessage = "Gorev aciklamasi en az 3 karakter olmalidir.")]
     public string Description { get; set; } = string.Empty;
     public TaskStatus Status { get; set; } =  TaskStatus.Ready;
     public bool IsDeleted { get; set; } =  false;
