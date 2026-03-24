f494bdc91b938d8d07f032ed15e395abff783bea
Phantomer-soft <kucuktas255@gmail.com>
2026-03-25 02:42:52 +0300

    update: updated login type from username to email

--- TaskManager/Data/Models/Dtos/UserDtos/LoginDto.cs
+++ TaskManager/Data/Models/Dtos/UserDtos/LoginDto.cs
@@ -4,8 +4,10 @@ namespace TaskManager.Data.Models.Dtos;
 
 public class LoginDto
 {
-    [MinLength(4,ErrorMessage = "Kullanici adi en az 4 karakter olmalidir")]
-    public string UserName { get; set; } =  string.Empty;
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } =  string.Empty;
+    [Required]
     [MinLength(6,ErrorMessage = "Sifre en az 6 karakter olmalidir")]
     public string Password { get; set; }  =  string.Empty;
 }
